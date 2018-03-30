3c5d25ab08b55fcc0f5e1ad37113a67a78395cd6
mikle <miklemv83@gmail.com>
2018-03-30 13:06:26 +0300

    fix order properties yaml

--- dbgit/src/main/java/ru/fusionsoft/dbgit/yaml/DBGitYamlRepresenter.java
+++ dbgit/src/main/java/ru/fusionsoft/dbgit/yaml/DBGitYamlRepresenter.java
@@ -1,6 +1,11 @@
 package ru.fusionsoft.dbgit.yaml;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
 import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.introspector.Property;
 import org.yaml.snakeyaml.nodes.Node;
 import org.yaml.snakeyaml.nodes.Tag;
 import org.yaml.snakeyaml.representer.Represent;
@@ -12,6 +17,20 @@ public class DBGitYamlRepresenter extends Representer {
 	public DBGitYamlRepresenter() {        
         this.representers.put(StringProperties.class, new RepresentYamlStringProperties());
     }
+	
+	@Override
+	protected Set<Property> getProperties(Class<? extends Object> type) {
+		
+		
+		if (type.equals(StringProperties.class)) {
+			//order for table 
+			Set<Property> result = new TreeSet<Property>(Collections.reverseOrder());
+	        result.addAll(super.getProperties(type));
+	        return result;
+		}
+
+		return super.getProperties(type);
+	}
 	
 	private class RepresentYamlStringProperties implements Represent {
 		public Node representData(Object data) {        
