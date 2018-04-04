5f78507b1f0b7509c452e89c6f5a9caaf473e39f
Vadim <molstih@yandex.ru>
2018-04-04 10:37:47 +0300

    3439 update CmdLink

--- dbgit/src/main/java/ru/fusionsoft/dbgit/command/CmdLink.java
+++ dbgit/src/main/java/ru/fusionsoft/dbgit/command/CmdLink.java
@@ -1,63 +1,31 @@
 package ru.fusionsoft.dbgit.command;
 
-import ru.fusionsoft.dbgit.core.DBConnection;
-import ru.fusionsoft.dbgit.core.DBGitPath;
 
-import java.io.File;
+import ru.fusionsoft.dbgit.core.DBGitPath;
 import java.io.FileWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class CmdLink implements IDBGitCommand {
 
 	public void execute(String[] args) {
 		// TODO Auto-generated method stub
-		if(args==null || args.length < 3) {
-			System.out.println("Args length = " + args.length);
-			for(String arg: args) {
-				System.out.println(arg);
-			}
+		if(args==null) {
+			System.out.println("Url database is empty");
+			
 		}else {
 			Connection conn = null;
 			String[] properties = CreateStringProperties(args);
 			Properties props = CreateProperties(properties);
+			
 			if(testingConnection(conn, args[0], props)) {
 				createFile(args[0], properties);
-				
 			}
 			else {
 				System.out.println("Connection no established!!!");
-				return;
 			}
 		}
-		
-		
-		/*
-		
-		System.out.println("excellent!!!");
-		Properties props = CreateProperties(CreateStringProperties(args));
-		try{
-			Connection conn = DriverManager.getConnection(args[0], props);
-			if(conn!=null) {
-				String dbGitPath=DBGitPath.getFullPath(DBGitPath.DB_INFO_FILE);
-				File file = new File(dbGitPath);
-			    
-				String filepath = file.getCanonicalPath();
-				System.out.println("Link On!!!");
-				System.out.println(filepath);
-			}
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
-		}*/
-		//создаем файл подключения
-		//DBConnection.createFileDBLink(/*params*/);
-		System.out.println("excellent!!!");
-		
-		//testConnect
-		//DBConnection.getInctance();
-		
 	}
 	
 	public Properties CreateProperties(String[] args) {
