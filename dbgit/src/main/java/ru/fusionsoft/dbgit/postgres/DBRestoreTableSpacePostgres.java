18acccef1d675cd541a408bc4e04129a664940b2
Nguyen Zoan Anh <hil4ik@mail.ru>
2018-04-18 13:05:56 +0300

    #3476 Restore User/Role without properties

---
+++ dbgit/src/main/java/ru/fusionsoft/dbgit/postgres/DBRestoreTableSpacePostgres.java
@@ -0,0 +1,20 @@
+package ru.fusionsoft.dbgit.postgres;
+
+import ru.fusionsoft.dbgit.adapters.DBRestoreAdapter;
+import ru.fusionsoft.dbgit.meta.IMetaObject;
+
+public class DBRestoreTableSpacePostgres extends DBRestoreAdapter{
+
+	@Override
+	public void restoreMetaObject(IMetaObject obj) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeMetaObject(IMetaObject obj) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
