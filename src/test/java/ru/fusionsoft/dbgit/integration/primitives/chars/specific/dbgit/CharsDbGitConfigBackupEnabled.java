0775fc7b641564d501d4f69b34d623c248bc66d8
rocket <gamingdas@gmail.com>
2021-06-04 18:49:07 +0300

    Add backups enabled DbGitIntegrationTestBasic#gitToDbRestoreWorks This scenario uses sequential restore and can cause some errors in backup adapters when running

---
+++ src/test/java/ru/fusionsoft/dbgit/integration/primitives/chars/specific/dbgit/CharsDbGitConfigBackupEnabled.java
@@ -0,0 +1,22 @@
+package ru.fusionsoft.dbgit.integration.primitives.chars.specific.dbgit;
+
+import ru.fusionsoft.dbgit.integration.primitives.chars.CharSequenceEnvelope;
+
+public class CharsDbGitConfigBackupEnabled extends CharSequenceEnvelope {
+    public CharsDbGitConfigBackupEnabled() {
+        super(() -> {
+            return "[core]\n"
+                   + "MAX_ROW_COUNT_FETCH = 10000\n"
+                   + "LIMIT_FETCH = true\n"
+                   + "LOG_ROTATE = 31\n"
+                   + "LANG = ENG\n"
+                   + "SCRIPT_ROTATE = 31\n"
+                   + "TO_MAKE_BACKUP = true\n"
+                   + "BACKUP_TO_SCHEME = true\n"
+                   + "BACKUP_TABLEDATA = true\n"
+                   + "PORTION_SIZE = 50000\n"
+                   + "TRY_COUNT = 1000\n"
+                   + "TRY_DELAY = 10\n";
+        });
+    }
+}
